ebab9f121998e962d75de957b6f4c40cce813054
David Buhauer <carbonex_92@hotmail.com>
2020-06-27 19:00:56 +0200

    add tls support again (nice to have when need to debug production code) + removed required column

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,14 +9,18 @@ public func configure(_ app: Application) throws {
     // Load enviroment if any
     Environment.dotenv()
     
-    try app.databases.use(.postgres(url: Environment.databaseURL), as: .psql)
+    // Heroku uses SSL verifcation when connecting to database.
+    // Below is therefor only necessary if you want to debug production database
+    var config = PostgresConfiguration(url: Environment.databaseURL)!
+    config.tlsConfiguration = TLSConfiguration.forClient(certificateVerification: .none)
+    app.databases.use(.postgres(configuration: config), as: .psql)
     
     // Tables
     app.migrations.add(CreateSubject())
     app.migrations.add(CreateDetail())
     
     // Fields
-    app.migrations.add(SubjectAddBackgroundColor(), to: .psql)
+    app.migrations.add(SubjectAddBackgroundColor())
 
     // register routes
     try routes(app)
